322a28e36fb82617d44587a0c92ced10d3639e98
Emeka Ofe <emekaofe@Emekas-MacBook-Pro.local>
2019-07-17 09:25:10 +0100

    add index.html page for rendering components

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,23 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './app/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist' ),
-        filename: 'index_bundle.js'
-    },
-    //For transforming our JSX and ES6 into normal JS
-    module : {
-        rules: [{test:/\.(js)$/, use: 'babel-loader'},
-        {test:/\.css$/, use: ['style-loader', 'css-loader']}
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js"
+  },
+  //For transforming our JSX and ES6 into normal JS
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
-    },
-    mode: 'development',
-
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: __dirname + './app/index.html',
-            filename: index.html
-        })
-    ]
-}
+  },
+  mode: "development",
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname + "/src/index.html")
+    })
+  ]
+};
